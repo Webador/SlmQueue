d77c7c867142527d61652e0f02e699cde54205cc
Jurian Sluiman <jurian@soflomo.com>
2012-04-05 11:04:02 +0200

    Add Pheanstalk as submodule

--- Module.php
+++ Module.php
@@ -19,4 +19,9 @@ class Module implements AutoloaderProvider
             ),
         );
     }
+    
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
 }
