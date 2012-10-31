6036e93b7e19313a7216ea4d72c0e3349aa61160
Jurian Sluiman <jurian@soflomo.com>
2012-10-31 15:48:50 +0100

    Make Pheanstalk bridge more flexible
    Implementation of exceptional cases with bury and release must happen
    outside the scope of the bridge. The service is now aware of a logger
    and a event manager, so it logs information at different priorities
    into a log and triggers events at every action of the beanstalk
    protocol.

--- src/SlmQueue/Service/PheanstalkBridge.php
+++ src/SlmQueue/Service/PheanstalkBridge.php
@@ -4,11 +4,34 @@ namespace SlmQueue\Service;
 
 use SlmQueue\Job\JobInterface;
 
-class PheanstalkBridge implements BeanstalkInterface
+use Zend\EventManager\EventManagerAwareInterface;
+use Zend\EventManager\EventManager;
+
+use Zend\Log\LoggerAwareInterface;
+use Zend\Log\Logger;
+
+class PheanstalkBridge implements
+    BeanstalkInterface,
+    EventManagerAwareInterface,
+    LoggerAwareInterface
 {
+    protected $pheanstalk;
+    protected $events;
+    protected $logger;
+
     public function __construct(Pheanstalk $pheanstalk)
     {
         $this->pheanstalk = $pheanstalk;
+    }
+
+    public function setEventManager(EventManagerInterface $events)
+    {
+        $this->events = $events;
+    }
+
+    public function setLogger(Logger $logger)
+    {
+        $this->logger = $logger;
     }
 
     /**
@@ -38,29 +61,48 @@ class PheanstalkBridge implements BeanstalkInterface
     {
         try {
             $job();
+            $this->log(Logger::DEBUG, sprintf('Job #%s executed (%s)', $job->getId, get_class($job)));
             $this->delete($job);
 
         } catch (ReleasableException $e) {
             $priority = method_exists($e, 'getPriority') ? $e->getPriority() : Pheanstalk::DEFAULT_PRIORITY;
             $delay    = method_exists($e, 'getDelay')    ? $e->getDelay()    : Pheanstalk::DEFAULT_DELAY;
 
-            $this->logException($job, $e);
-            $this->release($job, $priority, $delay, true);
+            $this->log(Logger::WARN,
+                       sprintf('Caught ReleasableException for job #%s (%s): %s',
+                               $job->getId(),
+                               get_class($job),
+                               get_class($e),
+                       ));
+            $this->release($job, $priority, $delay);
 
         } catch (BuryableException $e) {
             $priority = method_exists($e, 'getPriority') ? $e->getPriority() : Pheanstalk::DEFAULT_PRIORITY;
 
-            $this->logException($job, $e);
-            $this->bury($job, $priority, true);
+            $this->log(Logger::WARN,
+                       sprintf('Caught BuryableException for job #%s (%s): %s',
+                               $job->getId(),
+                               get_class($job),
+                               get_class($e),
+                       ));
+            $this->bury($job, $priority);
 
         } catch (Exception $e) {
-            $this->logException($job, $e);
-            $this->bury($job, 0, true);
+            $this->log(Logger::ERR,
+                       sprintf('Caught unknown exception for job #%s (%s): %s',
+                               $job->getId(),
+                               get_class($job),
+                               get_class($e),
+                       ));
+            $this->bury($job, 0);
         }
     }
 
     public function put(JobInterface $job, $priority = null, $delay = null, $ttr = null)
     {
+        $this->log(Logger::DEBUG, sprintf('Job #%s put (%s)', $job->getId(), get_class($job)));
+        $this->trigger('put', array('job' => $job));
+
         $data = Json::encode(array(
             'name'    => get_class($job),
             'options' => $job->getOptions(),
@@ -72,32 +114,33 @@ class PheanstalkBridge implements BeanstalkInterface
 
     public function delete(JobInterface $job)
     {
+        $this->log(Logger::DEBUG, sprintf('Job #%s delete (%s)', $job->getId(), get_class($job)));
+        $this->trigger('delete', array('job' => $job));
+
         $this->pheanstalk->delete($job);
     }
 
     public function release(JobInterface $job, $priority = null, $delay = null, $usePut = false)
     {
-        if ($usePut) {
-            $this->delete($job);
-            $this->put($job, $priority, $delay)
-        } else {
-            $this->pheanstalk->release($job, $priority, $delay);
-        }
+        $this->log(Logger::DEBUG, sprintf('Job #%s released (%s)', $job->getId(), get_class($job)));
+        $this->trigger('release', array('job' => $job));
+
+        $this->pheanstalk->release($job, $priority, $delay);
     }
 
     public function bury(JobInterface $job, $priority = null, $usePut = false)
     {
-        if ($usePut) {
-            $this->delete($job);
-            $this->put($job);
-            $this->bury($job, $priority);
-        } else {
-            $this->pheanstalk->bury($job, $priority);
-        }
+        $this->log(Logger::DEBUG, sprintf('Job #%s buried (%s)', $job->getId(), get_class($job)));
+        $this->trigger('bury', array('job' => $job));
+
+        $this->pheanstalk->bury($job, $priority);
     }
 
     public function kick(JobInterface $job)
     {
+        $this->log(Logger::DEBUG, sprintf('Job #%s kicked (%s)', $job->getId(), get_class($job)));
+        $this->trigger('kick', array('job' => $job));
+
         $this->pheanstalk->kick($job);
     }
 
@@ -110,31 +153,17 @@ class PheanstalkBridge implements BeanstalkInterface
         return $this->getJobManager()->get($name, $options);
     }
 
-    protected function logException(JobInterface $job, Exception $e)
+    protected function trigger($name, array $options)
     {
-        $options = $job->getOptions();
-
-        if (array_key_exists('loop_count', $options)) {
-            $options['loop_count']++;
-            $job->setOptions($options);
-
-            return $job;
+        if ($this->events instanceof EventManager) {
+            $this->events->trigger($name, $this, $options);
         }
+    }
 
-        /**
-         * @todo Store all exceptions, even for a second exception thrown in the cycle
-         */
-        $options['exception'] = array(
-            'type'    => get_class($e),
-            'message' => $e->getMessage(),
-            'trace'   => $e->getTraceAsString(),
-        );
-        /**
-         * @todo Log previous exceptions $e->getPrevious() and add the complete list
-         */
-        $options['loop_count'] = 1;
-        $job->setOptions($options);
-
-        return $job;
+    protected function log($priority, $message)
+    {
+        if ($this->logger instanceof Logger) {
+            $this->logger->log($priority, $message);
+        }
     }
 }
