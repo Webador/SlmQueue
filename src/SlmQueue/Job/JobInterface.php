513b4f83fd2cc42fc070850109bd3c293e824457
Jurian Sluiman <jurian@soflomo.com>
2012-11-01 22:54:15 +0100

    Add setter for id in interface

--- src/SlmQueue/Job/JobInterface.php
+++ src/SlmQueue/Job/JobInterface.php
@@ -7,5 +7,6 @@ interface JobInterface
     public function __invoke();
     public function setOptions (array $options);
     public function getOptions ();
+    public function setId($id);
     public function getId();
 }
