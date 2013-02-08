bd6ffd3450e2330115f6e692d98a0eefbf66c831
Michaël Gallego <mic.gallego@gmail.com>
2013-02-08 15:11:54 +0100

    Add tests for worker options

---
+++ tests/SlmQueueTest/Options/WorkerOptionsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SlmQueueTest\Options;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use SlmQueueTest\Util\ServiceManagerFactory;
+use Zend\ServiceManager\ServiceManager;
+
+class WorkerOptionsTest extends TestCase
+{
+    /**
+     * @var ServiceManager
+     */
+    protected $serviceManager;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->serviceManager = ServiceManagerFactory::getServiceManager();
+    }
+
+    public function testCreateWorkerOptions()
+    {
+        /** @var $workerOptions \SlmQueue\Options\WorkerOptions */
+        $workerOptions = $this->serviceManager->get('SlmQueue\Options\WorkerOptions');
+
+        $this->assertInstanceOf('SlmQueue\Options\WorkerOptions', $workerOptions);
+        $this->assertEquals(100000, $workerOptions->getMaxRuns());
+        $this->assertEquals(1024, $workerOptions->getMaxMemory());
+    }
+}
