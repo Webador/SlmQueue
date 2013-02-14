c898b48609c5d8f0da3fb4987deb485558944602
Michaël Gallego <mic.gallego@gmail.com>
2013-02-14 15:31:20 +0100

    LOL

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -17,9 +17,13 @@
  * <http://www.doctrine-project.org>.
  */
 
-$loader = include __DIR__ . '/../../../autoload.php';
+if  (
+    !($loader = @include __DIR__ . '/../vendor/autoload.php')
+    && !($loader = @include __DIR__ . '/../../../autoload.php')
+) {
+    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+}
 
-var_dump('ok');
 /* @var $loader \Composer\Autoload\ClassLoader */
 $loader->add('SlmQueueTest\\', __DIR__);
 
