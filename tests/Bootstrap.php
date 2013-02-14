9102a564777306634caa64677b3e68045857646a
Michaël Gallego <mic.gallego@gmail.com>
2013-02-14 15:52:42 +0100

    again

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -18,8 +18,8 @@
  */
 
 if  (
-    !($loader = @include __DIR__ . '/../vendor/autoload.php')
-    && !($loader = @include __DIR__ . '/../../../autoload.php')
+    !($loader = include __DIR__ . '/../vendor/autoload.php')
+    && !($loader = include __DIR__ . '/../../../autoload.php')
 ) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
