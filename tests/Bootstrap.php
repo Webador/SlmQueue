a93f48519b1b48aa5f89eb3b5ed4604a934aa1fa
Michaël Gallego <mic.gallego@gmail.com>
2013-02-14 15:12:29 +0100

    Another try

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -18,7 +18,7 @@
  */
 
 if  (
-    !($loader = @include __DIR__ . '/../vendor/autoload.php')
+    !($loader = @include __DIR__ . '/../../../../vendor/autoload.php')
     && !($loader = @include __DIR__ . '/../../../autoload.php')
 ) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
