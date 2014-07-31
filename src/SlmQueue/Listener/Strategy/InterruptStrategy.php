816c1582d7102a3905ff1f262330c8d7977d6ac0
Bas Kamer <bas@bushbaby.nl>
2014-07-31 16:23:48 +0200

    modified strategies

--- src/SlmQueue/Listener/Strategy/InterruptStrategy.php
+++ src/SlmQueue/Listener/Strategy/InterruptStrategy.php
@@ -14,30 +14,34 @@ class InterruptStrategy extends AbstractStrategy
 
     public function __construct()
     {
-        // Listen to the signals SIGTERM and SIGINT so that the worker can be killed properly. Note that
-        // because pcntl_signal may not be available on Windows, we needed to check for the existence of the function
-        if (function_exists('pcntl_signal')) {
+        if (function_exists('pcntl_signal')) { // Conditional because of lack of pcntl_signal on windows
             declare(ticks = 1);
-            pcntl_signal(SIGTERM, array($this, 'handleSignal'));
-            pcntl_signal(SIGINT,  array($this, 'handleSignal'));
+            pcntl_signal(SIGTERM, array($this, 'onPCNTLSignal'));
+            pcntl_signal(SIGINT,  array($this, 'onPCNTLSignal'));
         }
     }
 
     /**
      * {@inheritDoc}
      */
-    public function attach(EventManagerInterface $events)
+    public function attach(EventManagerInterface $events, $priority = 1)
     {
-        $this->listeners[] = $events->attach(WorkerEvent::EVENT_PROCESS_IDLE, array($this, 'onStopConditionCheck'));
-        $this->listeners[] = $events->attach(WorkerEvent::EVENT_PROCESS_JOB_POST, array($this, 'onStopConditionCheck'));
+        $this->listeners[] = $events->attach(WorkerEvent::EVENT_PROCESS_IDLE, array($this, 'onStopConditionCheck'), $priority);
+        $this->listeners[] = $events->attach(WorkerEvent::EVENT_PROCESS_JOB_POST, array($this, 'onStopConditionCheck'), $priority);
     }
 
+    /**
+     * Checks for the stop condition of this strategy
+     *
+     * @param WorkerEvent $event
+     * @return string
+     */
     public function onStopConditionCheck(WorkerEvent $event)
     {
         if ($this->interrupted) {
-            $event->stopPropagation(true);
+            $event->stopPropagation();
 
-            return 'interrupt by an external signal';
+            $this->exitState = sprintf("interrupt by an external signal on '%s'", $event->getName());
         }
     }
 
@@ -46,7 +50,7 @@ class InterruptStrategy extends AbstractStrategy
      *
      * @param int $signo
      */
-    public function handleSignal($signo)
+    public function onPCNTLSignal($signo)
     {
         switch($signo) {
             case SIGTERM:
@@ -55,5 +59,6 @@ class InterruptStrategy extends AbstractStrategy
                 break;
         }
     }
+
 
 }
