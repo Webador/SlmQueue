33d9e763cb756c35190c135d52a74e144bcf4adc
Bas Kamer <bas@bushbaby.nl>
2014-07-31 16:57:16 +0200

    adds LogJob strategy

---
+++ src/SlmQueue/Listener/Strategy/LogJobStrategy.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace SlmQueue\Listener\Strategy;
+
+use SlmQueue\Worker\WorkerEvent;
+use Zend\Console\Console;
+use Zend\EventManager\EventManagerInterface;
+
+class LogJobStrategy extends AbstractStrategy
+{
+    /**
+     * @var bool
+     */
+    protected $interrupted = false;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function attach(EventManagerInterface $events, $priority = 1)
+    {
+        $this->listeners[] = $events->attach(WorkerEvent::EVENT_PROCESS_JOB_PRE, array($this, 'onLogJobProcessStart'), $priority);
+        $this->listeners[] = $events->attach(WorkerEvent::EVENT_PROCESS_JOB_POST, array($this, 'onLogJobProcessDone'), $priority);
+    }
+
+    /**
+     * @param WorkerEvent $e
+     */
+    public function logJobProcessStart(WorkerEvent $e)
+    {
+        $job  = $e->getJob();
+        $name = $job->getMetadata('name');
+        if (null === $name) $name = get_class($job);
+
+        $console = Console::getInstance();
+        $console->write(sprintf('Processing job %s...', $name));
+    }
+
+    /**
+     * @param WorkerEvent $e
+     */
+    public function logJobProcessDone(WorkerEvent $e)
+    {
+        $console = Console::getInstance();
+        $console->writeLine('Done!');
+    }
+
+}
