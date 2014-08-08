fe8544bf61a4e8d498415f676d4453309f92a08c
Jurian Sluiman <jurian@soflomo.com>
2014-08-08 16:11:39 +0200

    Fix most of the worker tests with events
    Register the max runs strategy so the options are still taken
    into account and by default in the setup() all tests are setup
    that they quit after one worker cycle.

--- tests/SlmQueueTest/Asset/SimpleWorker.php
+++ tests/SlmQueueTest/Asset/SimpleWorker.php
@@ -6,12 +6,16 @@ use SlmQueue\Job\JobInterface;
 use SlmQueue\Queue\QueueInterface;
 use SlmQueue\Worker\AbstractWorker;
 use Zend\EventManager\EventManager;
+use Zend\EventManager\EventManagerInterface;
 
 class SimpleWorker extends AbstractWorker
 {
-    public function __construct()
+    public function __construct(EventManagerInterface $eventManager = null)
     {
-        parent::__construct(new EventManager);
+        if (null === $eventManager) {
+            $eventManager = new EventManager;
+        }
+        parent::__construct($eventManager);
     }
 
     public function processJob(JobInterface $job, QueueInterface $queue)
