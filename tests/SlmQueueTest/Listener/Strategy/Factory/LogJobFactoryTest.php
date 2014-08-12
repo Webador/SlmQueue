7d504968faad6769021ee59a16719bbb4a2130c5
Bas Kamer <bas@bushbaby.nl>
2014-08-12 15:21:26 +0200

    fix test

--- tests/SlmQueueTest/Listener/Strategy/Factory/LogJobFactoryTest.php
+++ tests/SlmQueueTest/Listener/Strategy/Factory/LogJobFactoryTest.php
@@ -4,6 +4,7 @@ namespace SlmQueueTest\Listener\Strategy\Factory;
 
 use PHPUnit_Framework_TestCase as TestCase;
 use SlmQueue\Listener\Strategy\Factory\LogJobStrategyFactory;
+use SlmQueue\Listener\StrategyPluginManager;
 use SlmQueueTest\Util\ServiceManagerFactory;
 
 class LogJobFactoryTest extends TestCase
@@ -11,9 +12,13 @@ class LogJobFactoryTest extends TestCase
 
     public function testCreateService()
     {
+        $plugin   = new StrategyPluginManager();
         $sm       = ServiceManagerFactory::getServiceManager();
+
+        $plugin->setServiceLocator($sm);
+
         $factory  = new LogJobStrategyFactory();
-        $strategy = $factory->createService($sm);
+        $strategy = $factory->createService($plugin);
 
         $this->assertInstanceOf('SlmQueue\Listener\Strategy\LogJobStrategy', $strategy);
     }
