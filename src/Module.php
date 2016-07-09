550d79579a60eb9919cb96c6eec813647d69ebee
Mike Zukowsky <michal@durooil.com>
2016-07-09 09:35:55 +0200

    Fix module config location

--- src/Module.php
+++ src/Module.php
@@ -15,6 +15,6 @@ class Module implements ConfigProviderInterface
      */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 }
