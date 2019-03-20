1551e9bc5c4bd511d4cd159c951975705c8f4bb7
Ivo Monteiro <ivomonteiro@gmail.com>
2019-03-20 22:01:13 +0000

    CS

--- tests/ConfigProviderTest.php
+++ tests/ConfigProviderTest.php
@@ -7,22 +7,22 @@ use PHPUnit_Framework_TestCase as TestCase;
 
 class ConfigProviderTest extends TestCase
 {
-public function testConfigProviderGetConfig()
-{
-$configProvider = new \SlmQueue\ConfigProvider();
-$config         = $configProvider();
+    public function testConfigProviderGetConfig()
+    {
+        $configProvider = new \SlmQueue\ConfigProvider();
+        $config         = $configProvider();
 
-static::assertNotEmpty($config);
-}
+        static::assertNotEmpty($config);
+    }
 
-public function testConfigEqualsToModuleConfig()
-{
-$module         = new Module();
-$moduleConfig   = $module->getConfig();
-$configProvider = new \SlmQueue\ConfigProvider();
-$config         = $configProvider();
+    public function testConfigEqualsToModuleConfig()
+    {
+        $module         = new Module();
+        $moduleConfig   = $module->getConfig();
+        $configProvider = new \SlmQueue\ConfigProvider();
+        $config         = $configProvider();
 
-static::assertEquals($moduleConfig['service_manager'], $config['dependencies']);
-static::assertEquals($moduleConfig['slm_queue'], $config['slm_queue']);
-}
+        static::assertEquals($moduleConfig['service_manager'], $config['dependencies']);
+        static::assertEquals($moduleConfig['slm_queue'], $config['slm_queue']);
+    }
 }
