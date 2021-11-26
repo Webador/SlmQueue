37128dfa9afea1bdc64ce9850d0d83f272fbeed2
Roel van Duijnhoven <roel@vanduijnhoven.com>
2021-11-26 09:58:05 +0100

    Typehint the Queue manager
    This will aid static analysis and IDE integration.

--- src/Queue/QueuePluginManager.php
+++ src/Queue/QueuePluginManager.php
@@ -4,6 +4,9 @@ namespace SlmQueue\Queue;
 
 use Laminas\ServiceManager\AbstractPluginManager;
 
+/**
+ * @method \SlmQueue\Queue\QueueInterface get($name, ?array $options = null)
+ */
 class QueuePluginManager extends AbstractPluginManager
 {
     public function validate($instance): void
